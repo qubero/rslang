a0037ed51400994f55b2dd0a1a05c6e7aa7f6320
Tolegen Timur <demigodkz@gmail.com>
2022-08-20 03:37:48 +0600

    feat: add component AppBar

---
+++ src/shared/components/AppBar/lib/styles.ts
@@ -0,0 +1,25 @@
+import MuiAppBar from '@mui/material/AppBar';
+import { styled } from '@mui/material/styles';
+import { IAppBar } from './types';
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open'
+})<IAppBar>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}));
+
+export { AppBar };
