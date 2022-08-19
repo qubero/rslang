03dcac00340eb05cfd21a124172fd40a581dfe7f
Tolegen Timur <demigodkz@gmail.com>
2022-08-20 03:47:36 +0600

    refactor: rename folder comptonents to ui

--- src/widgets/DrawerLayout/index.tsx
+++ src/widgets/DrawerLayout/index.tsx
@@ -2,9 +2,9 @@ import { memo } from 'react';
 import { Box, CssBaseline } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
-import MiniDrawer from 'shared/components/Drawer';
-import AppBar from 'shared/components/AppBar';
-import { DrawerHeader } from 'shared/components/Drawer/lib/styles';
+import MiniDrawer from 'shared/ui/Drawer';
+import AppBar from 'shared/ui/AppBar';
+import { DrawerHeader } from 'shared/ui/Drawer/lib/styles';
 import { useDrawer } from './model';
 import { drawlerStyle } from './lib/styled';
 
