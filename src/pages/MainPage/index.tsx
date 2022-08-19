0d60d9d66b709146afc22cf0e8dd478c0636913d
Tolegen Timur <demigodkz@gmail.com>
2022-08-20 03:41:23 +0600

    feat: add component MainPage

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from '@mui/material';
+import DrawerLayout from 'widgets/DrawerLayout';
+import Footer from 'widgets/Footer';
+
+const MainPage = () => {
+  return <Footer />;
+};
+
+export default MainPage;
