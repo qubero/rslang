021460707ccc56a4e3399cb068475123f683eebb
wolsos-v <mamaev757@gmail.com>
2022-08-22 11:14:57 +0300

    feat: implement games shared logic

---
+++ src/widgets/Games/model/hooks/useGameInit.ts
@@ -0,0 +1,37 @@
+import { useLayoutEffect, useState } from 'react';
+import { getRandomIntInclusive } from 'shared/lib';
+import { PAGE_PER_GROUP_COUNT } from '../constants';
+import useGameSettings from './useGameSettings';
+
+const useGameInit = () => {
+  const { getSettings } = useGameSettings();
+
+  const [settings, setSettings] = useState(getSettings());
+  const [isStart, setIsStart] = useState(false);
+
+  const handleStart = () => setIsStart(true);
+
+  const handleGroupChange = (newGroup: number) => {
+    const newPage = getRandomIntInclusive(0, PAGE_PER_GROUP_COUNT);
+    const newSettings = { group: newGroup, page: newPage };
+
+    setSettings((prev) => ({ ...prev, ...newSettings }));
+  };
+
+  useLayoutEffect(() => {
+    setSettings(getSettings());
+
+    return () => {
+      setIsStart(false);
+    };
+  }, [getSettings]);
+
+  return {
+    settings,
+    isStart,
+    handleStart,
+    handleGroupChange
+  };
+};
+
+export default useGameInit;
