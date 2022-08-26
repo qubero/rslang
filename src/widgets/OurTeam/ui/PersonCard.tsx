c6ca5f2a113a44ed12ea6a6118c5c4fb1444b554
Tolegen Timur <demigodkz@gmail.com>
2022-08-27 02:43:39 +0600

    refactor: change structure of component OurTeam

---
+++ src/widgets/OurTeam/ui/PersonCard.tsx
@@ -0,0 +1,37 @@
+import { Card, CardActionArea, CardMedia, CardContent, Typography, Divider } from '@mui/material';
+import { motion } from 'framer-motion';
+import { IPerson } from '../lib/types';
+import { getCardStyle } from '../lib/util';
+
+const PersonCard = (props: IPerson) => {
+  const { title, link, src, subtitle, description, duty, index, slideAnimation } = props;
+
+  return (
+    <Card
+      key={title}
+      sx={{ maxWidth: 800, textDecoration: 'none' }}
+      component={motion.a}
+      href={link}
+      variants={slideAnimation}
+      custom={(index + 1) * 2}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <CardActionArea sx={getCardStyle(index)}>
+        <CardMedia component="img" sx={{ width: 300, height: 350 }} image={src} alt={title} />
+        <CardContent>
+          <Typography variant="h5">{title}</Typography>
+          <Typography variant="body2" color="text.secondary" sx={{ mb: 1, ml: 0.3 }}>
+            {subtitle}
+          </Typography>
+          <Typography variant="body2">{description}</Typography>
+          <Divider sx={{ height: 2, mb: 1 }} />
+          <Typography variant="h6">Обязаности</Typography>
+          <Typography variant="body2">{duty}</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default PersonCard;
