8dbb7481aeea6cea4f3e2397947a78917f079bd2
wolsos-v <mamaev757@gmail.com>
2022-08-27 10:41:58 +0300

    fix: correct shared/utils export

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,6 +1,1 @@
-export const getRandomIntInclusive = (min: number, max: number): number => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+export { getRandomIntInclusive } from './utils';
