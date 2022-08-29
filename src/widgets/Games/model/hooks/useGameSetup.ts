2f11303ec9b3af309508baf20a3dcc5e240f9ea6
wolsos-v <mamaev757@gmail.com>
2022-08-29 23:20:48 +0300

    refactor: update aggregated words filter

--- src/widgets/Games/model/hooks/useGameSetup.ts
+++ src/widgets/Games/model/hooks/useGameSetup.ts
@@ -1,16 +1,16 @@
 import useAuth from 'widgets/Authorization/model/hooks/useAuth';
-import { useAggregatedWords, useNonAggregatedWords } from 'widgets/UserWords';
+import {
+  getAggregatedWordsFilter,
+  useAggregatedWords,
+  useNonAggregatedWords,
+} from 'widgets/UserWords';
 import { IGameSettings, MIN_WORDS_FOR_GAME_COUNT } from '../constants';
 
 const useGameSetup = (initSettings: IGameSettings) => {
   const { isAuth } = useAuth();
-  const aggregatedWords = useAggregatedWords({
-    group: initSettings.group,
-    page: initSettings.page,
-    wordsPerPage: MIN_WORDS_FOR_GAME_COUNT,
-    filter:
-      '{"$or":[{"userWord.difficulty":"medium"},{"userWord.difficulty":"hard"},{"userWord":null}]}',
-  });
+  const filter = getAggregatedWordsFilter(initSettings, null, false);
+
+  const aggregatedWords = useAggregatedWords({ wordsPerPage: MIN_WORDS_FOR_GAME_COUNT, filter });
   const nonAggregatedWords = useNonAggregatedWords(initSettings);
 
   return isAuth ? aggregatedWords : nonAggregatedWords;
---
+++ src/widgets/UserWords/model/utils.ts
@@ -0,0 +1,33 @@
+export const getAggregatedWordsFilter = (
+  params: { page: number; group: number } | null = null,
+  difficulty: string | null = null,
+  isLearned: boolean | null = null
+) => {
+  if (!params) {
+    if (difficulty !== null) {
+      return isLearned === null
+        ? `{ "$and": [{ "userWord.difficulty": "${difficulty}" }] }`
+        : `{ "$and": [{ "userWord.difficulty": "${difficulty}" }, { "userWord.optional.isLearned": ${isLearned} }] }`;
+    } else {
+      return isLearned === null
+        ? ``
+        : `{ "$and": [{ "userWord.optional.isLearned": ${isLearned} }] }`;
+    }
+  } else {
+    const { page, group } = params;
+
+    if (difficulty === null && isLearned === null) {
+      return `{ "$and": [{ "page": ${page} }, {"group": ${group} }] }`;
+    }
+
+    if (difficulty !== null) {
+      return isLearned === null
+        ? `{ "$and": [{ "page": ${page} }, {"group": ${group} }, { "userWord.difficulty": "${difficulty}" }] }`
+        : `{ "$and": [{ "page": ${page} }, {"group": ${group} }, { "userWord.difficulty": "${difficulty}" }, { "userWord.optional.isLearned": ${isLearned} }] }`;
+    } else {
+      return isLearned === null
+        ? `{ "$and": [{ "page": ${page} }, {"group": ${group} }] }`
+        : `{ "$and": [{ "page": ${page} }, {"group": ${group} }, { "userWord.optional.isLearned": ${isLearned} }] }`;
+    }
+  }
+};
