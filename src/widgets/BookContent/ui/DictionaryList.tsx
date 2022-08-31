34706521c30531b20a281b989e48236c29bc509b
Tolegen Timur <demigodkz@gmail.com>
2022-09-01 02:34:40 +0600

    feat: add component DictionaryList

---
+++ src/widgets/BookContent/ui/DictionaryList.tsx
@@ -0,0 +1,29 @@
+import { memo, useState } from 'react';
+import { Unstable_Grid2 as Grid } from '@mui/material';
+
+import useAuth from 'widgets/Authorization/model/hooks/useAuth';
+import { getAggregatedWordsFilter, useAggregatedWords } from 'widgets/UserWords';
+import WordsProgress from './WordsProgress';
+import WordItem from './WordItem';
+import DictionaryAuth from './DictionaryAuth';
+
+const DictionaryList = () => {
+  const { isAuth } = useAuth();
+  const [userId, setUserId] = useState('');
+  const filter = getAggregatedWordsFilter(null, 'hard');
+
+  const { data, isReady } = useAggregatedWords({ wordsPerPage: 3600, filter });
+
+  if (!isReady && isAuth) return <WordsProgress />;
+  if (!isAuth) return <DictionaryAuth />;
+
+  return (
+    <Grid container spacing={2} sx={{ width: '100%' }}>
+      {data.map((item, index) => (
+        <WordItem item={item} index={index} userId={userId} setUserId={setUserId} />
+      ))}
+    </Grid>
+  );
+};
+
+export default memo(DictionaryList);
