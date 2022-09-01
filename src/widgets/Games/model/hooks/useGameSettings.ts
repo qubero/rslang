a539d791c0f9a9541bf8d318c8ce74377edd44ee
wolsos-v <mamaev757@gmail.com>
2022-09-01 20:59:06 +0300

    refactor: remove unused logic

--- src/widgets/Games/model/hooks/useGameSettings.ts
+++ src/widgets/Games/model/hooks/useGameSettings.ts
@@ -3,16 +3,11 @@ import { useLocation } from 'react-router-dom';
 import { IGameSettings, INITIAL_GAME_SETTINGS } from '../constants';
 
 const useGameSettings = () => {
-  const { state, pathname } = useLocation();
+  const { state } = useLocation();
 
   const getSettingsByLocation = useCallback((): IGameSettings => {
-    if (pathname === '/book') {
-      // TODO: get values from url params
-      return { group: 0, page: 0, fromBook: true };
-    }
-
     return INITIAL_GAME_SETTINGS;
-  }, [pathname]);
+  }, []);
 
   const getSettings = useCallback((): IGameSettings => {
     return (state as IGameSettings) || INITIAL_GAME_SETTINGS;
